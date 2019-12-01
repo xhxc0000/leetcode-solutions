abcd4bc7007f1eb1d8ffb823891e24c46b6209aa
xhxc <xhxc0000@163.com>
2019-12-01 09:55:43 +0800

    add 二叉树的最低公共祖先, 层序遍历

--- tree/BuildTree.java
+++ tree/BuildTree.java
@@ -8,24 +8,29 @@ public class BuildTree {
   }
 
   // val -> index
-  static Map<Integer, Integer> map = new HashMap<>();
+  static Map<Integer, Integer> indexMap = new HashMap<>();
 
   private static TreeNode buildTree(int[] preorder, int[] inorder) {
+    if (preorder == null || inorder == null || preorder.length == 0 || inorder.length == 0) return null;
+
     for (int i = 0; i < inorder.length; i++) {
-      map.put(i, inorder[i]);
+        indexMap.put(inorder[i], i);
     }
 
-    return buildTree(preorder, inorder, 0, preorder.length - 1, 0, preorder.length - 1);
+    return buildTree(preorder, inorder, 0, preorder.length - 1, 0, inorder.length - 1);
   }
 
-  private static TreeNode buildTree(int[] preorder,
-  int[] inorder, int preL, int preR, int inL, int inR) {
-    TreeNode root = new TreeNode(preorder[preL]);
-    int index = map.get(preorder[preL]);
-    int len = index - inL;
-    root.left = buildTree(preorder, inorder, preL + 1, preL + len, inL, index - 1);
-    root.right = buildTree(preorder, inorder, preL + len + 1, preR, index + 1, inR);
+  private static TreeNode buildTree(int[] pre,
+  int[] in, int preL, int preR, int inL, int inR) {
 
-    return root;
+   if (preL > preR) return null;
+   TreeNode root = new TreeNode(pre[preL]);
+   int index = indexMap.get(pre[preL]);
+   int len = index - inL;
+
+   root.left = buildTree(pre, in, preL + 1, preL + len, inL, index - 1);
+   root.right = buildTree(pre, in, preL + len + 1, preR, index + 1, inR);
+
+   return root;
   }
 }
