a8b495a5f426149b7920bd59ea99876b5be4f75a
wo <xhxc0000@163.com>
2019-12-16 09:42:43 +0800

    add J240

---
+++ others/J240_Search2DMatrix2.java
@@ -0,0 +1,14 @@
+class J240_Search2DMatrix2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length < 1) return false;
+        int m = matrix.length, n = matrix[0].length;
+
+        for (int i = 0, j = n - 1; i < m && j >= 0;) {
+            if (matrix[i][j] == target) return true;
+            else if (matrix[i][j] > target) j--;
+            else i++;
+        }
+
+        return false;
+    }
+}
